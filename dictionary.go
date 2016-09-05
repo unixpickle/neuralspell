92e2b10a23056f0962bc3aa4ab4f1d4189158657
Alex Nichol <unixpickle@gmail.com>
2016-09-05 10:56:46 -0400

    return ctc.Samples from dictionary

--- dictionary.go
+++ dictionary.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/unixpickle/num-analysis/linalg"
 	"github.com/unixpickle/sgd"
-	"github.com/unixpickle/weakai/rnn/seqtoseq"
+	"github.com/unixpickle/speechrecog/ctc"
 )
 
 // A Dictionary is an sgd.SampleSet which can generate
@@ -69,50 +69,15 @@ func (d *Dictionary) Swap(i, j int) {
 // GetSample generates a seqtoseq.Sample for the indexed
 // entry in the dictionary.
 func (d *Dictionary) GetSample(i int) interface{} {
-	var phoneSeq []linalg.Vector
-	var spellingSeq []linalg.Vector
-
-	for _, phone := range d.Pronunciations[i] {
-		index, err := phoneIndex(phone)
-		if err != nil {
-			panic(err)
-		}
-		phoneVec := make(linalg.Vector, len(Phones)+1)
-		phoneVec[index] = 1
-		phoneSeq = append(phoneSeq, phoneVec)
-		repeatVec := make(linalg.Vector, len(phoneVec))
-		repeatVec[index] = 1
-		repeatVec[len(Phones)] = 1
-		for i := 0; i < phoneSequenceSpacing; i++ {
-			phoneSeq = append(phoneSeq, repeatVec)
-		}
-	}
-
-	for _, letter := range d.Spellings[i] {
-		index := int(letter) - int('a')
-		if index < 0 || index >= LetterCount {
-			panic("unknown letter: " + string(letter))
-		}
-		letterVec := make(linalg.Vector, LetterCount+1)
-		letterVec[index] = 1
-		spellingSeq = append(spellingSeq, letterVec)
-		repeatVec := make(linalg.Vector, len(letterVec))
-		letterVec[index] = 1
-		letterVec[LetterCount] = 1
-		for i := 0; i < letterSequenceSpacing; i++ {
-			spellingSeq = append(spellingSeq, repeatVec)
-		}
-	}
-
 	if d.InputPhones {
-		return seqtoseq.Sample{
-			Inputs:  phoneSeq,
-			Outputs: spellingSeq,
+		return ctc.Sample{
+			Input: d.phoneInput(i),
+			Label: d.letterOutput(i),
 		}
 	} else {
-		return seqtoseq.Sample{
-			Inputs:  spellingSeq,
-			Outputs: phoneSeq,
+		return ctc.Sample{
+			Input: d.letterInput(i),
+			Label: d.phoneOutput(i),
 		}
 	}
 }
@@ -136,4 +101,68 @@ func (d *Dictionary) Subset(start, end int) sgd.SampleSet {
 		Pronunciations: d.Pronunciations[start:end],
 		InputPhones:    d.InputPhones,
 	}
+}
+
+func (d *Dictionary) phoneInput(i int) []linalg.Vector {
+	var phoneSeq []linalg.Vector
+	for _, phone := range d.Pronunciations[i] {
+		index, err := phoneIndex(phone)
+		if err != nil {
+			panic(err)
+		}
+		phoneVec := make(linalg.Vector, len(Phones)+1)
+		phoneVec[index] = 1
+		phoneSeq = append(phoneSeq, phoneVec)
+		repeatVec := make(linalg.Vector, len(phoneVec))
+		repeatVec[index] = 1
+		repeatVec[len(Phones)] = 1
+		for i := 0; i < phoneSequenceSpacing; i++ {
+			phoneSeq = append(phoneSeq, repeatVec)
+		}
+	}
+	return phoneSeq
+}
+
+func (d *Dictionary) letterInput(i int) []linalg.Vector {
+	var spellingSeq []linalg.Vector
+	for _, letter := range d.Spellings[i] {
+		index := int(letter) - int('a')
+		if index < 0 || index >= LetterCount {
+			panic("unknown letter: " + string(letter))
+		}
+		letterVec := make(linalg.Vector, LetterCount+1)
+		letterVec[index] = 1
+		spellingSeq = append(spellingSeq, letterVec)
+		repeatVec := make(linalg.Vector, len(letterVec))
+		letterVec[index] = 1
+		letterVec[LetterCount] = 1
+		for i := 0; i < letterSequenceSpacing; i++ {
+			spellingSeq = append(spellingSeq, repeatVec)
+		}
+	}
+	return spellingSeq
+}
+
+func (d *Dictionary) phoneOutput(i int) []int {
+	var phoneSeq []int
+	for _, phone := range d.Pronunciations[i] {
+		index, err := phoneIndex(phone)
+		if err != nil {
+			panic(err)
+		}
+		phoneSeq = append(phoneSeq, index)
+	}
+	return phoneSeq
+}
+
+func (d *Dictionary) letterOutput(i int) []int {
+	var letterSeq []int
+	for _, letter := range d.Spellings[i] {
+		index := int(letter) - int('a')
+		if index < 0 || index >= LetterCount {
+			panic("unknown letter: " + string(letter))
+		}
+		letterSeq = append(letterSeq, index)
+	}
+	return letterSeq
 }
